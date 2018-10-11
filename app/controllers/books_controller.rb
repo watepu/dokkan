fdb5bea3eb05ab3d1874c6eca50c8f478698fe8c
watepu <40508321+watepu@users.noreply.github.com>
2018-10-11 18:36:52 +0900

    new,create,index追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,27 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :headline, :content)
+  end
+
+
 end
