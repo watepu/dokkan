3c5105bacdee6a718f7703ffc0b9ad8347c0aaa2
watepu <40508321+watepu@users.noreply.github.com>
2018-10-11 12:21:10 +0900

    bookモデル、コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  resources :books
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
