e3d8031bfe001ffb5b333626409d825b2dfffcb4
watepu <40508321+watepu@users.noreply.github.com>
2019-01-16 12:23:11 +0900

    rspecの初期設定

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,16 @@ module Dokkan
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
-
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
