4ba61fc383de22d81d0aa74aed5e58f82ebdca34
watepu <40508321+watepu@users.noreply.github.com>
2019-01-19 15:33:11 +0900

    テストデータの作成

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :book do
-    
+    title { 'BOOK1' }
+    headline { 'ruby' }
+    content { 'RSpecについて' }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'テストユーザー' }
+    email { 'test1@dic.com' }
+    password { 'password' }
   end
 end
