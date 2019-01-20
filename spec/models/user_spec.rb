45aac078fbb9ac5338628a1c783391160ac508e7
watepu <40508321+watepu@users.noreply.github.com>
2019-01-20 18:07:09 +0900

    テスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe User, type: :model do
     user_a = FactoryBot.create(:user)
     expect(user_a).to be_valid
   end
+
+  it 'emailがなければエラー' do
+    # user_b = FactoryBot.create(:user, email: '')
+    user_b = User.new(email: nil)
+    user_b.valid?
+    expect(user_b.errors[:email]).to include("を入力してください")
+  end
+
+  it 'emailが重複しているとエラー' do
+    user_a = FactoryBot.create(:user)
+    user_c = User.new(email: 'test3@example.com')
+    user_c.valid?
+    expect(user_c.errors[:email]).to include("はすでに存在します")
+  end
 end
