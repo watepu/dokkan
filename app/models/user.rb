fca55661437ac32c9242bc173a0259674e095574
watepu <40508321+watepu@users.noreply.github.com>
2019-03-10 14:22:44 +0900

    タブ、スペース調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,17 @@
 class User < ApplicationRecord
-
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i(google)
+         :omniauthable, omniauth_providers: %i[google]
 
   def self.find_for_google(auth)
     user = User.find_by(email: auth.info.email)
 
-    unless user
-      user = User.new(email: auth.info.email,
+    user ||= User.new(email: auth.info.email,
                       provider: auth.provider,
-                      uid:      auth.uid,
-                      password: Devise.friendly_token[0, 20],
-                                   )
-    end
+                      uid: auth.uid,
+                      password: Devise.friendly_token[0, 20])
     user.save
     user
   end
