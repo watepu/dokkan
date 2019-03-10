48e699ada6f8949a2b756eb296a3bd9e3b23396c
watepu <40508321+watepu@users.noreply.github.com>
2019-03-10 14:08:36 +0900

    uid,providerカラムをusersに追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181028100436) do
+ActiveRecord::Schema.define(version: 20190310045925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20181028100436) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.text "image"
+    t.string "provider", default: "", null: false
+    t.string "uid", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
